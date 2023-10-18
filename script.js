9e64936890ef1e3b7ba181a1dceccf3b86fc1697
SanjaySanz <145100090+SanjaySanz@users.noreply.github.com>
2023-10-18 18:51:06 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,86 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit',(e)=> {
+    
+    if(!validateInputs()){
+        e.preventDefault();
+
+    }
+
+
+})
+
+function validateInputs(){
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    let success = true
+
+    if(usernameValue === '') {
+
+        success = false
+
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        success = false
+
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+
+        success = false
+        setError(email, 'Provide a valid email address')
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        success = false
+
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+
+        success = false
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    return success;
+
+}
+
+function setError(element, message){
+
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+
+}
+
+function setSuccess(element){
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = (email) => {
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+
+};
+
